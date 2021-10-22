ba17676ddf36ac1384fbd5021e7755a7ac574491
hafidz0098 <hafidznak123@gmail.com>
2021-10-22 15:04:24 +0800

    update copyright

--- resources/views/dashboard/layouts/footerdash.blade.php
+++ resources/views/dashboard/layouts/footerdash.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; DDBGTS 2021</span>
+            <span>Copyright &copy; PIT DDBGTS 2022</span>
         </div>
     </div>
 </footer>
