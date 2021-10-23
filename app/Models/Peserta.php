ef38546a25949b3fb55aca9a2317dee613970602
hafidz0098 <hafidznak123@gmail.com>
2021-10-24 03:30:41 +0800

    add peserta,kategori for table and model

---
+++ app/Models/Peserta.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Peserta extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function kategori(){
+        return $this->belongsTo(Kategori::class);
+    }
+    
+}
---
+++ database/migrations/2021_10_23_191523_create_peserta_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesertaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peserta', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kategori_peserta_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('image');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peserta');
+    }
+}
