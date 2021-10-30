34b7afadd4cf4da5483cd0a245e6533fe0606a44
hafidz0098 <hafidznak123@gmail.com>
2021-10-31 03:14:36 +0800

    Update sidebardash.blade.php

--- resources/views/dashboard/layouts/sidebardash.blade.php
+++ resources/views/dashboard/layouts/sidebardash.blade.php
@@ -42,6 +42,7 @@
     <div class="sidebar-heading">
         Admin
     </div>
+    
 
     <li class="nav-item {{ Request::is('dashboard/categories*') ? 'active' : '' }}">
         <a class="nav-link" href="/dashboard/categories">
@@ -50,6 +51,22 @@
     </li>
 
     <li class="nav-item {{ Request::is('dashboard/peserta-pending*') ? 'active' : '' }}">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-users"></i>
+            <span>Peserta</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="/dashboard/peserta-pending">Peserta Pending</a>
+                <a class="collapse-item" href="/dashboard/peserta-sukses">Peserta Sukses</a>
+                <a class="collapse-item" href="/dashboard/peserta-gagal">Peserta Gagal</a>
+            </div>
+        </div>
+    </li>
+
+    {{-- <li class="nav-item {{ Request::is('dashboard/peserta-pending*') ? 'active' : '' }}">
         <a class="nav-link" href="/dashboard/peserta-pending">
             <i class="fas fa-user-clock"></i>
             <span>Peserta Pending</span></a>
@@ -65,7 +82,7 @@
         <a class="nav-link" href="/dashboard/peserta-gagal">
             <i class="fas fa-user-times"></i>
             <span>Peserta Gagal</span></a>
-    </li>
+    </li> --}}
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
