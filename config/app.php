275400549094052099cc400b324c91640d24fcec
hafidz0098 <hafidznak123@gmail.com>
2021-11-18 12:55:09 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -27,6 +27,7 @@ return [
     */
 
     'env' => env('APP_ENV', 'production'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     /*
     |--------------------------------------------------------------------------
