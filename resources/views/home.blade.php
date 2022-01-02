b2276b366b59dc1be6f143d5af5879656e55fbb0
hafidz0098 <hafidznak123@gmail.com>
2022-01-02 22:03:30 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -379,8 +379,6 @@
           <img src="frontend/assets/img/clients/infokupedia.webp" class="img-fluid" alt="logo infokupedia">
         </div>
 
-        
-
         <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
           <img src="frontend/assets/img/bone_media.webp" class="img-fluid" alt="logo berita kota" width="60px">
         </div>
