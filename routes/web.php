25bf7166b3a4105094326d68e9a39f71e31ee64e
alfonsus20 <alfonschandrawan@gmail.com>
2022-01-02 14:18:26 +0800

    disable register route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\EmailController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,82 +23,78 @@ use App\Http\Controllers\EmailController;
 */
 
 Route::get('/', function () {
-    return view ('home', [
+    return view('home', [
         "title" => "DDBGTS 2022",
         'active' => 'home',
     ]);
 });
 
 Route::get('/faq', function () {
-    return view ('faq', [
+    return view('faq', [
         "title" => "FAQ",
         'active' => 'faq',
     ]);
 });
 
 Route::get('/cara-pendaftaran', function () {
-    return view ('cara-pendaftaran', [
+    return view('cara-pendaftaran', [
         "title" => "Cara Pendaftaran",
         'active' => 'cara-pendaftaran',
     ]);
 });
 
 
-Route::get('/berita', [PostController::class, 'index']); 
+Route::get('/berita', [PostController::class, 'index']);
 
 Route::get('/berita/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/exportpeserta', 'PesertaController@pesertaexport')->name('exportpeserta')->middleware('auth'); 
+Route::get('/exportpeserta', 'PesertaController@pesertaexport')->name('exportpeserta')->middleware('auth');
 
 
-Route::group(['middleware' => 'revalidate'], function()
-{
- 
-Auth::routes();
+Route::group(['middleware' => 'revalidate'], function () {
 
-Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+    Auth::routes(['register' => false]);
 
-Route::post('/login', [LoginController::class, 'authenticate']);
+    Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 
-Route::post('/logout', [LoginController::class, 'logout']);
+    Route::post('/login', [LoginController::class, 'authenticate']);
+
+    Route::post('/logout', [LoginController::class, 'logout']);
 
 
-Route::get('/daftar', 'DaftarController@index')->middleware('guest');
-Route::post('/daftar', 'DaftarController@store');
+    Route::get('/daftar', 'DaftarController@index')->middleware('guest');
+    Route::post('/daftar', 'DaftarController@store');
 
-// Route::get('/email', 'EmailController@index');
+    // Route::get('/email', 'EmailController@index');
 
-Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
+    Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
 
-Route::get('/dashboard/posts/checkSlug', 'DashboardPostController@checkSlug')->middleware('auth');
+    Route::get('/dashboard/posts/checkSlug', 'DashboardPostController@checkSlug')->middleware('auth');
 
-Route::resource('/dashboard/posts', 'DashboardPostController')->middleware('auth');  
+    Route::resource('/dashboard/posts', 'DashboardPostController')->middleware('auth');
 
-Route::resource('/dashboard/peserta-pending', 'PesertaController')->middleware('admin');
+    Route::resource('/dashboard/peserta-pending', 'PesertaController')->middleware('admin');
 
-Route::resource('/dashboard/peserta-sukses', 'PesertaSuksesController')->middleware('admin');
+    Route::resource('/dashboard/peserta-sukses', 'PesertaSuksesController')->middleware('admin');
 
-Route::resource('/dashboard/peserta-gagal', 'PesertaGagalController')->middleware('admin');
+    Route::resource('/dashboard/peserta-gagal', 'PesertaGagalController')->middleware('admin');
 
-Route::resource('/dashboard/ticketbox', 'TicketboxController')->middleware('admin');
+    Route::resource('/dashboard/ticketbox', 'TicketboxController')->middleware('admin');
 
-Route::get('/dashboard/email/webinar', 'EmailController@viewWebinar')->middleware('admin');
-Route::post('/dashboard/email/webinar', 'EmailController@kirimWebinar')->middleware('admin');
-Route::put('/dashboard/email/webinar', 'EmailController@kirimWebinar')->middleware('admin');
+    Route::get('/dashboard/email/webinar', 'EmailController@viewWebinar')->middleware('admin');
+    Route::post('/dashboard/email/webinar', 'EmailController@kirimWebinar')->middleware('admin');
+    Route::put('/dashboard/email/webinar', 'EmailController@kirimWebinar')->middleware('admin');
 
-Route::get('/dashboard/email/talentsmapping', 'EmailController@viewtm')->middleware('admin');
-Route::post('/dashboard/email/talentsmapping', 'EmailController@kirimtm')->middleware('admin');
-Route::put('/dashboard/email/talentsmapping', 'EmailController@kirimtm')->middleware('admin');
+    Route::get('/dashboard/email/talentsmapping', 'EmailController@viewtm')->middleware('admin');
+    Route::post('/dashboard/email/talentsmapping', 'EmailController@kirimtm')->middleware('admin');
+    Route::put('/dashboard/email/talentsmapping', 'EmailController@kirimtm')->middleware('admin');
 
-Route::get('/dashboard/email/tryout', 'EmailController@viewTryout')->middleware('admin');
-Route::post('/dashboard/email/tryout', 'EmailController@kirimTryout')->middleware('admin');
-Route::put('/dashboard/email/tryout', 'EmailController@kirimTryout')->middleware('admin');
+    Route::get('/dashboard/email/tryout', 'EmailController@viewTryout')->middleware('admin');
+    Route::post('/dashboard/email/tryout', 'EmailController@kirimTryout')->middleware('admin');
+    Route::put('/dashboard/email/tryout', 'EmailController@kirimTryout')->middleware('admin');
 
-Route::post('/dashboard/ticketbox', 'TicketboxController@store')->middleware('admin');
+    Route::post('/dashboard/ticketbox', 'TicketboxController@store')->middleware('admin');
 
-Route::get('/dashboard/daftaradmin', [RegisterController::class, 'index'])->middleware('admin');
-Route::post('/dashboard/daftaradmin', [RegisterController::class, 'store'])->middleware('admin');
-
+    Route::get('/dashboard/daftaradmin', [RegisterController::class, 'index'])->middleware('admin');
+    Route::post('/dashboard/daftaradmin', [RegisterController::class, 'store'])->middleware('admin');
 });
-
-
