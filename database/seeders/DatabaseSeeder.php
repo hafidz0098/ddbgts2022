4952fea948f87f79db3230a28d2fd5bf1ee1ba5e
hafidz0098 <hafidznak123@gmail.com>
2022-01-26 16:54:11 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,64 +18,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        User::create([
-            'name' => 'Hafidz',
-            'username' => 'hafidz_hafidz',
-            'email' => 'hafidznak123@gmail.com',
-            'password' => bcrypt('valodulugan@123'),
-            'is_admin' => 1
-        ]);
-
-        User::create([
-            'name' => 'Alfonsus Avianto',
-            'username' => 'alfonsus',
-            'email' => 'alfonsusaviantochandrawan@gmail.com',
-            'password' => bcrypt('sayangeurico69'),
-            'is_admin' => 1
-        ]);
-
-        User::create([
-            'name' => 'Alfian',
-            'username' => 'alfian',
-            'email' => 'Alfiantriwahyus@gmail.com',
-            'password' => bcrypt('Valoterus123'),
-            'is_admin' => 1
-        ]);
-
-        User::create([
-            'name' => 'Rifky',
-            'username' => 'Rifky',
-            'email' => 'rifky.ilsss19@gmail.com',
-            'password' => bcrypt('gamingnonstop19'),
-            'is_admin' => 1
-        ]);
-
-        User::create([
-            'name' => 'Delisya',
-            'username' => 'delisya',
-            'email' => 'Delisyajayadi@gmail.com',
-            'password' => bcrypt('kochengoren12'),
-            'is_admin' => 1
-        ]);
-
-        User::create([
-            'name' => 'Agil',
-            'username' => 'agil',
-            'email' => 'Agilwnaldi@gmail.com',
-            'password' => bcrypt('kangcarrypit'),
-            'is_admin' => 1
-        ]);
-
-        User::create([
-            'name' => 'Alifiah',
-            'username' => 'alifiah',
-            'email' => 'Alifiahmaharani123@gmail.com',
-            'password' => bcrypt('Hapisganteng123'),
-            'is_admin' => 1
-        ]);
-
-
 
         // User::factory(3)->create();
 
